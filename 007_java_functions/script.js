12082c520f4897dd95c873ed49f6afb2acc83b56
Jason Summers <94763652+SummersResearch@users.noreply.github.com>
2023-08-03 20:01:12 -0700

    updated JavaScript files

---
+++ 007_java_functions/script.js
@@ -0,0 +1,11 @@
+
+
+// var greeting = "Hi!  My name is Jason Summers";
+// console.log(greeting);
+
+var testExample = function () {
+    var greeting = "Hi!  My name is Jason";
+    return  greeting;
+}
+
+console.log(testExample());
